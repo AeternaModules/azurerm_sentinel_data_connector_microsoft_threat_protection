84e83a69cbbae2e2977c3cd824f097195fdc27b1
Daniel Allen <dan@aeternagroup.net>
2026-01-11 18:05:40 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "sentinel_data_connector_microsoft_threat_protections" {
+  description = "All sentinel_data_connector_microsoft_threat_protection resources"
+  value       = azurerm_sentinel_data_connector_microsoft_threat_protection.sentinel_data_connector_microsoft_threat_protections
+}
+
