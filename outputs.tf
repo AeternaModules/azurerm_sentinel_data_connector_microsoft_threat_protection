a72bdd596baf14d56956f0eb4f79c4b787322b5d
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:57 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "sentinel_data_connector_microsoft_threat_protections" {
   description = "All sentinel_data_connector_microsoft_threat_protection resources"
   value       = azurerm_sentinel_data_connector_microsoft_threat_protection.sentinel_data_connector_microsoft_threat_protections
 }
+output "sentinel_data_connector_microsoft_threat_protections_log_analytics_workspace_id" {
+  description = "List of log_analytics_workspace_id values across all sentinel_data_connector_microsoft_threat_protections"
+  value       = [for k, v in azurerm_sentinel_data_connector_microsoft_threat_protection.sentinel_data_connector_microsoft_threat_protections : v.log_analytics_workspace_id]
+}
+output "sentinel_data_connector_microsoft_threat_protections_name" {
+  description = "List of name values across all sentinel_data_connector_microsoft_threat_protections"
+  value       = [for k, v in azurerm_sentinel_data_connector_microsoft_threat_protection.sentinel_data_connector_microsoft_threat_protections : v.name]
+}
+output "sentinel_data_connector_microsoft_threat_protections_tenant_id" {
+  description = "List of tenant_id values across all sentinel_data_connector_microsoft_threat_protections"
+  value       = [for k, v in azurerm_sentinel_data_connector_microsoft_threat_protection.sentinel_data_connector_microsoft_threat_protections : v.tenant_id]
+}
 
