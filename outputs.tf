9522eda02c6f948c3a2f147f40d68145de900cb4
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:48 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "sentinel_data_connector_microsoft_threat_protections_id" {
+  description = "Map of id values across all sentinel_data_connector_microsoft_threat_protections, keyed the same as var.sentinel_data_connector_microsoft_threat_protections"
+  value       = { for k, v in azurerm_sentinel_data_connector_microsoft_threat_protection.sentinel_data_connector_microsoft_threat_protections : k => v.id }
+}
 output "sentinel_data_connector_microsoft_threat_protections_log_analytics_workspace_id" {
   description = "Map of log_analytics_workspace_id values across all sentinel_data_connector_microsoft_threat_protections, keyed the same as var.sentinel_data_connector_microsoft_threat_protections"
   value       = { for k, v in azurerm_sentinel_data_connector_microsoft_threat_protection.sentinel_data_connector_microsoft_threat_protections : k => v.log_analytics_workspace_id }
