d0698d7f40b1a2019caf681a9702996d89804fa1
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:14 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "sentinel_data_connector_microsoft_threat_protections" {
-  description = "All sentinel_data_connector_microsoft_threat_protection resources"
-  value       = azurerm_sentinel_data_connector_microsoft_threat_protection.sentinel_data_connector_microsoft_threat_protections
-}
 output "sentinel_data_connector_microsoft_threat_protections_log_analytics_workspace_id" {
-  description = "List of log_analytics_workspace_id values across all sentinel_data_connector_microsoft_threat_protections"
-  value       = [for k, v in azurerm_sentinel_data_connector_microsoft_threat_protection.sentinel_data_connector_microsoft_threat_protections : v.log_analytics_workspace_id]
+  description = "Map of log_analytics_workspace_id values across all sentinel_data_connector_microsoft_threat_protections, keyed the same as var.sentinel_data_connector_microsoft_threat_protections"
+  value       = { for k, v in azurerm_sentinel_data_connector_microsoft_threat_protection.sentinel_data_connector_microsoft_threat_protections : k => v.log_analytics_workspace_id }
 }
 output "sentinel_data_connector_microsoft_threat_protections_name" {
-  description = "List of name values across all sentinel_data_connector_microsoft_threat_protections"
-  value       = [for k, v in azurerm_sentinel_data_connector_microsoft_threat_protection.sentinel_data_connector_microsoft_threat_protections : v.name]
+  description = "Map of name values across all sentinel_data_connector_microsoft_threat_protections, keyed the same as var.sentinel_data_connector_microsoft_threat_protections"
+  value       = { for k, v in azurerm_sentinel_data_connector_microsoft_threat_protection.sentinel_data_connector_microsoft_threat_protections : k => v.name }
 }
 output "sentinel_data_connector_microsoft_threat_protections_tenant_id" {
-  description = "List of tenant_id values across all sentinel_data_connector_microsoft_threat_protections"
-  value       = [for k, v in azurerm_sentinel_data_connector_microsoft_threat_protection.sentinel_data_connector_microsoft_threat_protections : v.tenant_id]
+  description = "Map of tenant_id values across all sentinel_data_connector_microsoft_threat_protections, keyed the same as var.sentinel_data_connector_microsoft_threat_protections"
+  value       = { for k, v in azurerm_sentinel_data_connector_microsoft_threat_protection.sentinel_data_connector_microsoft_threat_protections : k => v.tenant_id }
 }
 
